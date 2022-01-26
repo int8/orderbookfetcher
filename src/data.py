02529c313d93c774899ad2305e11c7570ea191ed
kamil <kamil@czarnogorski.pl>
2022-01-26 08:12:45 +0100

    data loader

--- src/data.py
+++ src/data.py
@@ -1,11 +1,14 @@
 import os
+from abc import abstractmethod, ABC
+
+import numpy as np
 from torch.utils.data import Dataset, DataLoader
 import random
 
 from src.model import OrderBooksDataSequenceDatasetV1
 
 
-class OrderBooksDataset(Dataset):
+class OrderBooksSequenceDatasetBase(Dataset, ABC):
     def __init__(self, data_directory, epoch_size: int,
                  files_to_merge: int = 10, time_length=100,
                  min_sequence_length_in_file=1000):
@@ -16,7 +19,6 @@ class OrderBooksDataset(Dataset):
         self.files = os.listdir(data_directory)
         self.file_loaded_counts = dict(zip(self.files, [0] * len(self.files)))
         self.loaded_data = []
-
         self.last_file_read = 0
         self.min_sequence_length_in_file = min_sequence_length_in_file
 
@@ -42,6 +44,10 @@ class OrderBooksDataset(Dataset):
                 if i >= self.files_to_merge:
                     break
 
+    @abstractmethod
+    def x(self, sparse_matrices, idx):
+        pass
+
     def mark_as_read(self, f):
         self.file_loaded_counts[f] += 1
 
@@ -52,14 +58,38 @@ class OrderBooksDataset(Dataset):
         if idx == 0:
             self.load_to_memory()
 
-        x, y, s3_keys, metadata = self.loaded_data[self.last_file_read]
+        sparse_matrices, y, s3_keys, metadata = self.loaded_data[
+            self.last_file_read]
         self.last_file_read = (self.last_file_read + 1) % len(self.loaded_data)
         i = random.randint(1, len(s3_keys) - self.time_length)
-        # TODO: return proper matrices
         return (
-            y[:, i: (i + self.time_length)].astype(float),
+            self.x(sparse_matrices, i),
             y[:, i: (i + self.time_length)].astype(float)
         )
+
+
+class OrderBooksSequenceDataset(OrderBooksSequenceDatasetBase):
+    def __init__(self, data_directory, epoch_size: int,
+                 files_to_merge: int = 10, time_length=100,
+                 min_sequence_length_in_file=1000, x_shape=(64, 64),
+                 flattened=False):
+        super().__init__(data_directory, epoch_size, files_to_merge,
+                         time_length, min_sequence_length_in_file)
+        self.x_shape = x_shape
+        self.flattened = flattened
+
+    def x(self, sparse_matrices, idx):
+        m = np.zeros(shape=(2,) + self.x_shape + (self.time_length,))
+        for i in range(self.time_length):
+            bids_sparse = sparse_matrices['bids_sparse'][idx + i]
+            asks_sparse = sparse_matrices['asks_sparse'][idx + i]
+            m[0, bids_sparse[0], bids_sparse[1], i] = bids_sparse[2]
+            m[1, asks_sparse[0], asks_sparse[1], i] = asks_sparse[2]
+        if self.flattened:
+            return m.reshape(2 * self.x_shape[0] * self.x_shape[1],
+                             self.time_length)
+        else:
+            return m
 
 
 class OrderBooksDataLoader(DataLoader):
